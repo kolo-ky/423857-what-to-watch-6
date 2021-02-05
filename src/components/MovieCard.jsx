c14a919efe9d3bf20b6c328be6e08ac6041e2e25
Alexander <kolo-ky@yandex.ru>
2021-02-05 20:49:18 +0500

    fix props and components render

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const MovieCard = ({movieInfo}) => {
-  const {title, img} = movieInfo;
-
-  return <article className="small-movie-card catalog__movies-card">
-    <div className="small-movie-card__image">
-      <img src={img} alt={title} width="280" height="175"/>
-    </div>
-    <h3 className="small-movie-card__title">
-      <a className="small-movie-card__link" href="movie-page.html">{title}</a>
-    </h3>
-  </article>;
+const MovieCard = ({title, img}) => {
+  return (
+    <article className="small-movie-card catalog__movies-card">
+      <div className="small-movie-card__image">
+        <img src={img} alt={title} width="280" height="175"/>
+      </div>
+      <h3 className="small-movie-card__title">
+        <a className="small-movie-card__link" href="movie-page.html">{title}</a>
+      </h3>
+    </article>
+  );
 };
 
 MovieCard.propTypes = {
-  movieInfo: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    img: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired
-  })
+  img: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
 };
 
 export default MovieCard;
