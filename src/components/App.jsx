c038646d880226abc789d43fd8688bb3ef6d7a18
Alexander <kolo-ky@yandex.ru>
2021-02-06 14:38:45 +0500

    added react router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
-import Main from './pages/Main';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
-const App = (props) => <Main {...props}/>;
+// components
+import Main from './pages/Main';
+import Login from './pages/Login';
+import Film from './pages/Film';
+import AddReview from './pages/AddReview';
+import MyList from './pages/MyList';
+import Player from './pages/Player';
+import NotFound from './pages/NotFound';
+
+const App = (props) => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact
+          render={() => (
+            <Main {...props}/>
+          )}
+        />
+        <Route path="/login" exact component={Login} />
+        <Route path="/mylist" exact component={MyList} />
+        <Route path="/films/:id" exact component={Film} />
+        <Route path="/films/:id/review" exact component={AddReview} />
+        <Route path="/player/:id" exact component={Player} />
+        <Route component={NotFound}/>
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
